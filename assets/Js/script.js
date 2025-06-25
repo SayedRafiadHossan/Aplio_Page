dcca7c755a36a68bac94b5cb21a4a1d236fa9c3e
Rafiad <rafiadhoseen@gmail.com>
2025-06-25 23:23:49 +0600

    nav bar complete..

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -7,3 +7,17 @@ function openSearchModal() {
   const search = document.getElementById("search-container");
   search.style.display = "block";
 }
+
+function autoNavbar() {
+  //   console.log(window.scrollY);
+  const navBar = document.getElementById("full-navbar");
+  if (window.scrollY > 100) {
+    navBar.style.backgroundColor = "rgba(0, 0, 0, 0.1)";
+    navBar.style.backdropFilter = "blur(10px)";
+    navBar.style.padding = "5px 0px";
+  } else {
+    navBar.style.backgroundColor = "rgba(0, 0, 0, 0)";
+    navBar.style.backdropFilter = "blur(0px)";
+    navBar.style.padding = "20px 0px";
+  }
+}
